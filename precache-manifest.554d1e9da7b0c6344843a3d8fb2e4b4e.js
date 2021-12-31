f31c366efe1e2886e14e0851a41118fcc262704b
HenrySantamariaC <hsantamariaca@unprg.edu.pe>
2021-12-30 23:04:04 -0500

    Despliegue

---
+++ precache-manifest.554d1e9da7b0c6344843a3d8fb2e4b4e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61574b06e04b1c8cd665",
+    "url": "/SuncardFirebase/css/app.824eddb7.css"
+  },
+  {
+    "revision": "78b00c01ea0683d8424e87b4bd882296",
+    "url": "/SuncardFirebase/img/Login semicircle.78b00c01.svg"
+  },
+  {
+    "revision": "beedb25e3e5be72196ac09c2878e7856",
+    "url": "/SuncardFirebase/img/assets/icomoon/fonts/icomoon.eot"
+  },
+  {
+    "revision": "eabca1ec9e7b746294a2e03cd383be50",
+    "url": "/SuncardFirebase/img/assets/icomoon/fonts/icomoon.svg"
+  },
+  {
+    "revision": "2a268d658d02b4734aebd71a4fda272c",
+    "url": "/SuncardFirebase/img/assets/icomoon/fonts/icomoon.ttf"
+  },
+  {
+    "revision": "2c55132e674bf6a40a458247f56395e5",
+    "url": "/SuncardFirebase/img/assets/icomoon/fonts/icomoon.woff"
+  },
+  {
+    "revision": "1bf6c422f45390e1fd292beec541ce73",
+    "url": "/SuncardFirebase/img/assets/icomoon/style.css"
+  },
+  {
+    "revision": "0a53d18f7dffa7afd9b94bf2decf2b11",
+    "url": "/SuncardFirebase/img/default.0a53d18f.png"
+  },
+  {
+    "revision": "589074ef2dd2b6908c867335fc17a221",
+    "url": "/SuncardFirebase/img/user-1.589074ef.png"
+  },
+  {
+    "revision": "e53239d0a36f201bd4d38183a2d28638",
+    "url": "/SuncardFirebase/img/user-2.e53239d0.png"
+  },
+  {
+    "revision": "8a6f4de2252a511bbf2c2b065f8a294e",
+    "url": "/SuncardFirebase/img/user-3.8a6f4de2.png"
+  },
+  {
+    "revision": "1c82128003219e56ceb0378e0b9a86b9",
+    "url": "/SuncardFirebase/img/user-4.1c821280.png"
+  },
+  {
+    "revision": "1022d1a349ccd2e0b902cc7252405a59",
+    "url": "/SuncardFirebase/img/user-5.1022d1a3.png"
+  },
+  {
+    "revision": "56299bed64eced63ff41ef28c03bde90",
+    "url": "/SuncardFirebase/img/user-6.56299bed.png"
+  },
+  {
+    "revision": "c133b4a0e0c4d0d3ae2d02abec9949fc",
+    "url": "/SuncardFirebase/index.html"
+  },
+  {
+    "revision": "61574b06e04b1c8cd665",
+    "url": "/SuncardFirebase/js/app.42aef796.js"
+  },
+  {
+    "revision": "35cbd97f76f3e313a151",
+    "url": "/SuncardFirebase/js/chunk-vendors.3d254f3c.js"
+  },
+  {
+    "revision": "f968cf08d97adb7921daa1f166ceb75c",
+    "url": "/SuncardFirebase/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/SuncardFirebase/robots.txt"
+  }
+]);
